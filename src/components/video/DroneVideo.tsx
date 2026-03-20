56a4bcf01fe30e69b311d9fa0014a99dedd86142
seva <vsevolod.synytskyi@gmail.com>
2026-03-20 20:18:55 +0200

    slight video transition

--- src/components/video/DroneVideo.tsx
+++ src/components/video/DroneVideo.tsx
@@ -2,6 +2,7 @@ import { type FC, useEffect, useRef } from 'react'
 import { useVideoReceiver } from '@/hooks/useVideoReceiver.ts'
 import { Badge } from '@/components/ui/badge'
 import useVideoSender from '@/hooks/useVideoSender.ts'
+import { motion } from 'motion/react'
 
 export const DroneVideo: FC = () => {
   const { connect, status, remoteStream } = useVideoReceiver()
@@ -28,7 +29,8 @@ export const DroneVideo: FC = () => {
       >
         <Badge>{status}</Badge>
       </div>
-      <video
+      <motion.video
+        animate={{ opacity: status === 'streaming' ? 1 : 0 }}
         className="relative z-10 h-full w-full object-cover"
         ref={videoRef}
         autoPlay
